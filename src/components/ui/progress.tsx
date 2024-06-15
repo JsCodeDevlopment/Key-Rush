9d2ba1a006433c722f8691f8cfc769f7ab42adc1
JsCodeDevlopment <jonatasilva118@gmail.com>
2024-06-15 16:30:15 -0300

    refac: Update progress bar background color

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,7 +12,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-[#111111]",
+      "relative h-4 w-full overflow-hidden rounded-full bg-black",
       className
     )}
     {...props}
