70c01cd4fe9c49bdcea5a20622ec758de332c375
JsCodeDevlopment <jonatasilva118@gmail.com>
2024-06-15 12:12:48 -0300

    chore: Update character page UI styling

--- src/components/created-characters.tsx
+++ src/components/created-characters.tsx
@@ -9,7 +9,7 @@ export function CreatedCharacters() {
   return (
     <>
       <Link href={"/game"}>
-        <div className="flex flex-col items-center p-2 bg-[#ff3434] rounded-md">
+        <div className="flex flex-col h-48 w-32 items-center justify-between p-2 bg-[#ff3434] rounded-md">
           <Image
             src={male0}
             className="w-36"
@@ -17,10 +17,10 @@ export function CreatedCharacters() {
             height={200}
             alt="character1"
           />
-          <p className="text-[#111111]">Fire Hands</p>
+          <p className="text-white">Fire Hands</p>
         </div>
       </Link>
-      <div className="flex flex-col items-center p-2 bg-[#ff3434] rounded-md">
+      <div className="flex flex-col h-48 w-32 items-center justify-between p-2 bg-[#ff3434] rounded-md">
         <Image
           src={male3}
           className="w-36"
@@ -28,9 +28,9 @@ export function CreatedCharacters() {
           height={200}
           alt="character1"
         />
-        <p className="text-[#111111]">King of Words</p>
+        <p className="text-white">King of Words</p>
       </div>
-      <div className="flex flex-col items-center p-2 bg-[#ff3434] rounded-md">
+      <div className="flex flex-col h-48 w-32 items-center justify-between p-2 bg-[#ff3434] rounded-md">
         <Image
           src={female3}
           className="w-36"
@@ -38,9 +38,9 @@ export function CreatedCharacters() {
           height={200}
           alt="character1"
         />
-        <p className="text-[#111111]">Queen of Words</p>
+        <p className="text-white">Queen of Words</p>
       </div>
-      <div className="flex flex-col items-center p-2 bg-[#ff3434] rounded-md">
+      <div className="flex flex-col h-48 w-32 items-center justify-between p-2 bg-[#ff3434] rounded-md">
         <Image
           src={female4}
           className="w-36"
@@ -48,7 +48,7 @@ export function CreatedCharacters() {
           height={200}
           alt="character1"
         />
-        <p className="text-[#111111]">Word Warrior</p>
+        <p className="text-white">Word Warrior</p>
       </div>
     </>
   );
